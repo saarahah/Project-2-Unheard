b9abdd7317588a058625e89e14781eb718d55c4a
saarahah <sarahlferguson07@gmail.com>
2020-07-23 10:05:02 -0400

    fixed es lint

--- public/js/user.js
+++ public/js/user.js
@@ -4,6 +4,8 @@ const userName = document.querySelector(".userName");
 // Get current User and display email information.
 // Here you can display any User information coming from the server.
 fetch("/auth/user")
-    .then(response => response.json())
-    .then(data => { userName.textContent = data.email; })
-    .catch(err => console.log(err));
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.email;
+  })
+  .catch(err => console.log(err));
