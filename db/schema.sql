09e44899e842c02ff5d9e31a99dcab320cc65818
Paul DelPadre <PDelPadre@rice.local>
2020-07-24 09:59:24 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,23 +2,28 @@ CREATE DATABASE DB_DATABASE;
 
 USE DB_DATABASE;
 
-CREATE TABLE user
+CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar(255) NOT NULL,
 	password varchar(255) NOT NULL,
+	state VARCHAR(2);
+	city varchar(20) NOT NULL,
 	createdAt (datetime),
 	updatedAt (datetime),
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE blog
+CREATE TABLE posts
 (
 	id int NOT NULL AUTO_INCREMENT,
-	story text(65535) NOT NULL,
-	alias varchar(20) NOT NULL,
+	title varchar(255) NOT NULL,
+	body text(65535) NOT NULL,
+	createdAt (datetime),
+	updatedAt (datetime),
 	PRIMARY KEY (id)
 );
+
 
 CREATE TABLE death
 (
