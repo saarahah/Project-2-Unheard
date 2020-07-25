c4817657548304fe371367cfe62149e2f4e971dd
Paul DelPadre <PDelPadre@rice.local>
2020-07-25 02:21:01 -0400

    Submit button and routes not working for profile updates

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -8,3 +8,40 @@ const usrProfileStateUpdate = document.querySelector("#usrProfileStateUpdate");
 // const rowAlertReg = document.querySelector(".alertAppendReg");
 // const divAlertReg = document.createElement("div");
 
+
+
+
+// Send the PUT request.
+// $.ajax("/api/update/" + id, {
+//     type: "PUT",
+//     data: newSleepState
+// }).then(
+//     function () {
+//         console.log("changed sleep to", newSleep);
+//         // Reload the page to get the updated list
+//         location.reload();
+//     }
+// );
+
+// Submit Update Button for email
+$("submitBtnReg").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+    // packaging as object
+    var updateEmail = {
+        email: $("#usrProfileEmailUpdate").val().trim(),
+    };
+console.log(updateEmail);
+    // Send the put request.
+    $.ajax("/api/update/email/", {
+        // change to get
+        type: "PUT",
+        data: email
+    }).then(
+        function () {
+            console.log("Updated Users Email");
+            // Reload the page to get the updated list
+            location.reload();
+        }
+    );
+});
