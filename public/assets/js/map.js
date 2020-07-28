5523aae490cbf61669142ecdecf1ddb52ef5bd97
Cantando <65988365+Cantando@users.noreply.github.com>
2020-07-27 23:00:00 -0400

    placed geo location of user and fixed sql

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -2,13 +2,41 @@
 let states = [];
 let deathScale = 0;
 let response = [];
+var map, infoWindow;
 
 function initMap() {
   var usaLatlng = new google.maps.LatLng(39.381266, -97.922211);
-  var mapOptions = {
-    zoom: 4,
+  map = new google.maps.Map(document.getElementById("map"),{
     center: usaLatlng,
-  };
+    zoom: 4
+  });
+  
+  //  {
+  //   zoom: 4,
+  //   center: usaLatlng,
+  // };
+
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
   // var iconBase =
   //   "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
 
@@ -17,8 +45,6 @@ function initMap() {
       icon: "https://storage.googleapis.com/support-kms-prod/SNP_2752068_en_v0",
     },
   };
-
-  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   //testing
   // console.log("markers " + markerArray);
@@ -108,3 +134,14 @@ function initMap() {
     }
   });
 }
+
+
+//function referencing user geoLoc Err//
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
